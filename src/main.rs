f8a16638501c0bde7fbd4c918c47603a070ce880
Edgar Albalate <dev.gardo@gmail.com>
2019-05-06 12:37:29 +0200

    Alter version

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 
 fn main() {
-  let target = env::args().nth(1)
-               .map(|name| name)
-               .unwrap_or_else(|| "world".to_owned());
+  let target = match env::args().nth(1) {
+               Some(name) => name,
+               None => "world".to_owned()
+               };
   println!("Hello, {}!", target);
 }
